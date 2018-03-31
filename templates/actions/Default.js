6f4dbe5f64dee92a7ab96834a50c9126859c1dcd
jimib <jimi.bailey@gmail.com>
2018-03-31 17:13:14 +0100

    Added comments

--- templates/actions/Default.js
+++ templates/actions/Default.js
@@ -2,11 +2,13 @@ import {
 	LOGIN_FAILURE,
 } from './Types';
 
+//ADD COMMENTS HERE
 export const loginSuccess = data => ({
   type: LOGIN_SUCCESS,
   payload: data
 });
 
+//AND ADD COMMENTS HERE TOO
 export const loginFailed = data => ({
   type: LOGIN_FAILURE,
   payload: data
